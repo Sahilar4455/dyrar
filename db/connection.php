37d9bde0845b8764c8cbfa6edfe3877290ef1f7c
Sahilar4455 <sahil.ar4455@gmail.com>
2025-06-11 19:09:47 +0000

    Add files via upload

---
+++ db/connection.php
@@ -0,0 +1,13 @@
+<?php
+$host = "localhost";
+$dbname = "lp";
+$username = "root";
+$password = "12345";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
+?>
